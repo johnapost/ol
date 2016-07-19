bf0ed1b3c730dc456e08dda1de1ff3de0303a904
johnapost <notimetobeidle@gmail.com>
2016-07-19 00:32:40 -0400

    Add routing components to the precompile array of app component to get rid of console warning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,11 @@ import { HTTP_PROVIDERS } from '@angular/http'
 import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button'
 import { MD_TOOLBAR_DIRECTIVES } from '@angular2-material/toolbar'
 import { ROUTER_DIRECTIVES } from '@angular/router'
+import { BusinessesComponent, BusinessComponent } from './businesses'
 
 @Component({
   directives: [ROUTER_DIRECTIVES, MD_BUTTON_DIRECTIVES, MD_TOOLBAR_DIRECTIVES],
+  precompile: [BusinessesComponent, BusinessComponent],
   providers: [HTTP_PROVIDERS],
   selector: 'app',
   styleUrls: ['../components/app.component.css'],
